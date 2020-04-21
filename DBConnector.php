95602ae2f724a18a264997c87d949ad1ce73d0a2
AJ-Kulundu <52794633+AJ-Kulundu@users.noreply.github.com>
2020-04-21 22:23:47 +0300

    Add files via upload

---
+++ DBConnector.php
@@ -0,0 +1,24 @@
+<?php
+define('DB_SERVER','localhost');//local machine
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','btc3205');
+
+class DBConnector{
+    public $conn;
+    function __construct(){
+      $this->conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME) or die("Error:".mysqli_error() );
+      
+    }
+
+    public function CloseDatabase(){
+        mysqli_close($this->conn);
+    }
+}
+
+
+
+
+
+
+?>
